b924eabdb91c87b37b3f7d0c16a8eca60f12594d
Madd6 <aw251724@gmail.com>
2025-01-27 10:11:25 +0700

    commit vercel.json Change

--- api/app.js
+++ api/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const Order = require("./models/Order");
+const Order = require("../models/Order");
 require("dotenv").config();
 
 const app = express();
