f51905be71771bf80080d5aa547458f6bdefa988
Madd6 <aw251724@gmail.com>
2025-01-27 12:03:02 +0700

    commit schema change

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ app.delete("/orders/:orderId", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-// module.exports = app;
+module.exports = app;
